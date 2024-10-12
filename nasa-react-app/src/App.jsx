d13751806c549d11cfddfe5c18b1bfb2212fbf29
dmmalawliya <dmmalawliya@gmail.com>
2024-10-12 16:43:00 +0530

    integrated api, cache storage

--- nasa-react-app/src/App.jsx
+++ nasa-react-app/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/Footer"
+import SideBar from "./components/SideBar"
+import Main from "./components/Main"
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [ data,setData] = useState(null)
+
+  const [loading,setLoading] = useState(false)
+
+  const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+
+   const [ showModal, setShowModal] = useState(false)
+   
+
+   function  handleToggleModal() {
+    setShowModal(!showModal)
+   }
+
+   useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+
+      const today = (new Date()).toDateString()
+      const localKey = `NASA-${today}`
+
+
+      const cachedData = localStorage.getItem(localKey);
+      if (cachedData) {
+        try {
+          const apiData = JSON.parse(cachedData); // Ensure you're parsing JSON correctly
+          setData(apiData);
+          setLoading(false);
+          console.log('Fetched from cache today');
+          return;
+        } catch (error) {
+          console.error("Failed to parse cached data:", error);
+          // If parsing fails, you might want to clear this item
+          localStorage.removeItem(localKey);
+        }
+      }
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData)); // Ensure the data is stringified
+        setData(apiData);
+        console.log('Fetched from API today');
+      } catch (err) {
+        console.error(err.message);
+        // Optionally handle error state here
+      } finally {
+        setLoading(false); // Update loading state after API call
+      }
+    }
+
+
+
+    
+     fetchAPIData();
+   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+       {data ? (<Main data= { data} />) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+       )}
+       {showModal && <SideBar data={ data } handleToggleModal={handleToggleModal}/>} 
+       {data && (<Footer data= { data} handleToggleModal={handleToggleModal} />)}
+       </>
   )
 }
 
 export default App
+
+
+
